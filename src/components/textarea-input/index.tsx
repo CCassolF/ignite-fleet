3317e213837ee536953dc5c9773edc3586e0c108
Carlos Cassol <carloscassolfilho@gmail.com>
2024-08-31 20:33:15 -0300

    feat: add usability in inputs

--- src/components/textarea-input/index.tsx
+++ src/components/textarea-input/index.tsx
@@ -1,4 +1,5 @@
-import { TextInputProps } from 'react-native'
+import { forwardRef } from 'react'
+import { TextInput, TextInputProps } from 'react-native'
 import { useTheme } from 'styled-components/native'
 
 import { Container, Input, Label } from './styles'
@@ -7,18 +8,25 @@ export interface TextareaInputProps extends TextInputProps {
   label: string
 }
 
-export function TextareaInput({ label, ...rest }: TextareaInputProps) {
-  const { COLORS } = useTheme()
+const TextareaInput = forwardRef<TextInput, TextareaInputProps>(
+  ({ label, ...rest }, ref) => {
+    const { COLORS } = useTheme()
 
-  return (
-    <Container>
-      <Label>{label}</Label>
-      <Input
-        placeholderTextColor={COLORS.GRAY_400}
-        multiline
-        autoCapitalize="sentences"
-        {...rest}
-      />
-    </Container>
-  )
-}
+    return (
+      <Container>
+        <Label>{label}</Label>
+        <Input
+          ref={ref}
+          placeholderTextColor={COLORS.GRAY_400}
+          multiline
+          autoCapitalize="sentences"
+          {...rest}
+        />
+      </Container>
+    )
+  },
+)
+
+TextareaInput.displayName = 'TextareaInput'
+
+export { TextareaInput }
--- src/screens/departure/index.tsx
+++ src/screens/departure/index.tsx
@@ -1,3 +1,7 @@
+import { useRef } from 'react'
+import { TextInput } from 'react-native'
+
+import { Button } from '@/components/button'
 import { Header } from '@/components/header'
 import { LicensePlateInput } from '@/components/license-plate-input'
 import { TextareaInput } from '@/components/textarea-input'
@@ -7,16 +11,33 @@ import { Container, Content } from './styles'
 export interface DepartureProps {}
 
 export function Departure() {
+  const descriptionRef = useRef<TextInput>(null)
+
+  function handleDepartureRegister() {
+    console.log('Ok!')
+  }
+
   return (
     <Container>
       <Header title="Saída" />
 
       <Content>
-        <LicensePlateInput label="Placa do veículo" placeholder="BRA-1234" />
+        <LicensePlateInput
+          label="Placa do veículo"
+          placeholder="BRA-1234"
+          onSubmitEditing={() => descriptionRef.current?.focus()}
+          returnKeyType="next"
+        />
         <TextareaInput
+          ref={descriptionRef}
           label="Finalidade"
           placeholder="Vou utilizar o veículo para..."
+          onSubmitEditing={handleDepartureRegister}
+          returnKeyType="send"
+          blurOnSubmit
         />
+
+        <Button title="Registrar Saída" onPress={handleDepartureRegister} />
       </Content>
     </Container>
   )
