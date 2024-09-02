cd0438ae21151f5a8df54f330e8412a2a390a80d
Carlos Cassol <carloscassolfilho@gmail.com>
2024-09-02 00:21:53 -0300

    feat: display vehicle details

--- src/screens/arrival/index.tsx
+++ src/screens/arrival/index.tsx
@@ -30,6 +30,8 @@ export function Arrival() {
   const historic = useObject(Historic, new BSON.UUID(id) as unknown as string)
   const realm = useRealm()
 
+  const title = historic?.status === 'departure' ? 'Chegada' : 'Detalhes'
+
   function handleRemoveVehicleUsage() {
     Alert.alert('Cancelar', 'Cancelar a utilização do veículo?', [
       { text: 'Não', style: 'cancel' },
@@ -68,7 +70,7 @@ export function Arrival() {
 
   return (
     <Container>
-      <Header title="Chegada" />
+      <Header title={title} />
       <Content>
         <Label>Placa do veículo</Label>
 
@@ -78,10 +80,12 @@ export function Arrival() {
 
         <Description>{historic?.description}</Description>
 
-        <Footer>
-          <ButtonIcon icon={X} onPress={handleRemoveVehicleUsage} />
-          <Button title="Registrar chegada" onPress={handleArrivalRegister} />
-        </Footer>
+        {historic?.status === 'departure' && (
+          <Footer>
+            <ButtonIcon icon={X} onPress={handleRemoveVehicleUsage} />
+            <Button title="Registrar chegada" onPress={handleArrivalRegister} />
+          </Footer>
+        )}
       </Content>
     </Container>
   )
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -29,6 +29,10 @@ export function Home() {
     return navigate('arrival', { id: vehicleInUse._id.toString() })
   }
 
+  function handleHistoricDetails(id: string) {
+    navigate('arrival', { id })
+  }
+
   useEffect(() => {
     function fetchVehicleInUse() {
       try {
@@ -93,7 +97,12 @@ export function Home() {
         <FlatList
           data={vehicleHistoric}
           keyExtractor={(item) => item.id}
-          renderItem={({ item }) => <HistoricCard data={item} />}
+          renderItem={({ item }) => (
+            <HistoricCard
+              data={item}
+              onPress={() => handleHistoricDetails(item.id)}
+            />
+          )}
           showsVerticalScrollIndicator={false}
           contentContainerStyle={{ paddingBottom: 100 }}
           ListEmptyComponent={<Label>Nenhum registro de utilização.</Label>}
