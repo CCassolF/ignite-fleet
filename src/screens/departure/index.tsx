3c5572484058b1f91c3faf8a5ecc4f4e75ac1999
Carlos Cassol <carloscassolfilho@gmail.com>
2024-09-03 20:23:09 -0300

    feat: Getting the coordinates of the current location

--- src/screens/departure/index.tsx
+++ src/screens/departure/index.tsx
@@ -1,6 +1,11 @@
 import { useNavigation } from '@react-navigation/native'
 import { useUser } from '@realm/react'
-import { useForegroundPermissions } from 'expo-location'
+import {
+  LocationAccuracy,
+  LocationSubscription,
+  useForegroundPermissions,
+  watchPositionAsync,
+} from 'expo-location'
 import { useEffect, useRef, useState } from 'react'
 import {
   Alert,
@@ -82,6 +87,26 @@ export function Departure() {
   useEffect(() => {
     requestLocationForegroundPermission()
   }, [requestLocationForegroundPermission])
+
+  useEffect(() => {
+    if (!locationForegroundPermission?.granted) {
+      return
+    }
+
+    let subscription: LocationSubscription
+
+    watchPositionAsync(
+      {
+        accuracy: LocationAccuracy.High,
+        timeInterval: 1000,
+      },
+      (location) => {
+        console.log(location)
+      },
+    ).then((response) => (subscription = response))
+
+    return () => subscription.remove()
+  }, [locationForegroundPermission])
 
   if (!locationForegroundPermission?.granted) {
     return (
